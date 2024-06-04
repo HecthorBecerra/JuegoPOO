5c9b0c20aeedd1712d856b51f5c62c8ef30a7a8a
Arturo Rivas <a.rivas06@ufromail.cl>
2024-06-04 14:02:50 -0300

    Agrega constructores de Warrior y Wizard

--- src/main/java/domain/Warrior.java
+++ src/main/java/domain/Warrior.java
@@ -1,5 +1,12 @@
+package domain;
 public class Warrior extends Character {
+
 	private int strength;
+
+	public Warrior(String name,int hp,int attack,int defense, int strength) {
+		super(name, hp, attack, defense);
+		this.strength = strength;
+	}
 
 	public int attack(Character aCharacter) {
 		throw new UnsupportedOperationException();
