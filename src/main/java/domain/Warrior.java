817972f5cbeda84bde926fdf8a5c534c7044dd62
HectorBecerra <h.becerra01@ufromail.cl>
2024-06-04 18:22:35 -0400

    add methods for the class wizard and class warrior

--- src/main/java/domain/Warrior.java
+++ src/main/java/domain/Warrior.java
@@ -1,18 +1,22 @@
 package domain;
 public class Warrior extends Champion {
-
-	private int strength;
+	private final int strength;
 
 	public Warrior(String name,int hp,int attack,int defense, int strength) {
 		super(name, hp, attack, defense);
 		this.strength = strength;
 	}
 
-	public int attack(Champion aChampion) {
-		throw new UnsupportedOperationException();
+	public int attack(Champion enemy) {
+		int damage = this.attack + this.strength + this.weapon.getAttackPower() - this.defense;
+		if (damage < 0) {
+			damage = 0;
+		}
+		enemy.takeDamage(damage);
+		return damage;
 	}
 
-	public int calculateDamage(Champion aChampion) {
-		throw new UnsupportedOperationException();
+	public int calculateDamage(Champion enemy) {
+		return this.attack(enemy);
 	}
 }
--- src/main/java/domain/Wizard.java
+++ src/main/java/domain/Wizard.java
@@ -2,16 +2,21 @@ package domain;
 public class Wizard extends Champion {
 		private int magicPower;
 
-		Wizard(String name, int hp, int attack, int defense, int magicPower) {
+		public Wizard(String name, int hp, int attack, int defense, int magicPower) {
 			super(name, hp, attack, defense);
 			this.magicPower = magicPower;
 		}
 
-		public int attack(Champion aChampion) {
-			throw new UnsupportedOperationException();
+		public int attack(Champion enemy) {
+			int damage = this.attack + this.magicPower + this.weapon.getAttackPower() - this.defense;
+			if (damage < 0) {
+				damage = 0;
+			}
+			enemy.takeDamage(damage);
+			return damage;
 		}
 
-		public int calculateDamage(Champion aChampion) {
-			throw new UnsupportedOperationException();
+		public int calculateDamage(Champion enemy) {
+			return this.attack(enemy);
 		}
 }
