252e8c5cffbcd7ee73fcf75f0b2bdc9d8f2eb4bb
Arturo Rivas <a.rivas06@ufromail.cl>
2024-06-04 14:04:50 -0300

    Agrega constructor de Wizard

--- src/main/java/domain/Wizard.java
+++ src/main/java/domain/Wizard.java
@@ -1,5 +1,11 @@
+package domain;
 public class Wizard extends Character {
 		private int magicPower;
+
+		Wizard(String name, int hp, int attack, int defense, int magicPower) {
+			super(name, hp, attack, defense);
+			this.magicPower = magicPower;
+		}
 
 		public int attack(Character aCharacter) {
 			throw new UnsupportedOperationException();
