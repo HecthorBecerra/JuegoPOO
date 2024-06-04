51e9315dcb3d9a3429c30a9577f062bf75435e49
HectorBecerra <h.becerra01@ufromail.cl>
2024-06-04 13:09:05 -0400

    add constructor and attributes of the class Game.java

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,18 +1,31 @@
+import java.util.Random;
+
 public class Game {
 
-	public void startCombat(Character aCharacter1, Character aCharacter2) {
-		throw new UnsupportedOperationException();
-	}
+	private Character character1;
+	private Character character2;
+	private Random random;
 
-	private void attackTurn(Character aCharacterAttacks, Character aCharacterDefends) {
-		throw new UnsupportedOperationException();
-	}
-
-	private void declareWinner(Character aCharacter1, Character aCharacter2) {
-		throw new UnsupportedOperationException();
-	}
-
-	private void showMenu(Character aCharacter1, Character aCharacter2) {
-		throw new UnsupportedOperationException();
+	public Game(Character aCharacter1, Character aCharacter2) {
+		this.character1 = aCharacter1;
+		this.character2 = aCharacter2;
+		this.random = new Random();
 	}
 }
+
+
+/*public void startCombat(Character aCharacter1, Character aCharacter2) {
+	throw new UnsupportedOperationException();
+	}
+
+private void attackTurn(Character aCharacterAttacks, Character aCharacterDefends) {
+	throw new UnsupportedOperationException();
+}
+
+private void declareWinner(Character aCharacter1, Character aCharacter2) {
+	throw new UnsupportedOperationException();
+}
+
+private void showMenu(Character aCharacter1, Character aCharacter2) {
+	throw new UnsupportedOperationException();
+}*/
--- src/main/java/Wizard.java
+++ src/main/java/Wizard.java
@@ -1,11 +1,11 @@
-public class Wizard extends Character {
-	private int magicPower;
+	public class Wizard extends Character {
+		private int magicPower;
 
-	public int attack(Character aCharacter) {
-		throw new UnsupportedOperationException();
-	}
+		public int attack(Character aCharacter) {
+			throw new UnsupportedOperationException();
+		}
 
-	public int calculateDamage(Character aCharacter) {
-		throw new UnsupportedOperationException();
+		public int calculateDamage(Character aCharacter) {
+			throw new UnsupportedOperationException();
+		}
 	}
-}
