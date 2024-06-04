6ce46bdb55ff7eb81fd6e8f618ede7408f83089f
HectorBecerra <h.becerra01@ufromail.cl>
2024-06-04 13:56:25 -0400

    add constructor and attributes of the class Weapon.java

--- src/main/java/Weapon.java
+++ src/main/java/Weapon.java
@@ -4,6 +4,12 @@ public class Weapon {
 	private TypeDamage typeDamage;
 	public Character character;
 
+	public Weapon(String name, int attackPower, TypeDamage typeDamage) {
+		this.name = name;
+		this.attackPower = attackPower;
+		this.typeDamage = typeDamage;
+	}
+
 	public int getAttackPower() {
 		return this.attackPower;
 	}
